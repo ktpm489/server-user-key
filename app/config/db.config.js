c320253ec5425346fa283891fc37da16cc88ea4f
Hoàng Vũ Nguyễn <hoangvuit893@gmail.com>
2021-03-14 22:53:01 +0700

    Add data

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "bezkoder_db",
+  URL :'mongodb+srv://ktpm489:goodluck99@cluster0.hawap.mongodb.net/bezkoder_db?retryWrites=true&w=majority'
+};
