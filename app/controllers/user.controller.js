07704161a1561d721b5d00bddc4a57c259ac2d49
ktpm489 <hoangvu@bacoor.co>
2021-03-18 17:22:16 +0700

    Add user controller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -13,3 +13,118 @@ exports.adminBoard = (req, res) => {
 exports.moderatorBoard = (req, res) => {
   res.status(200).send("Moderator Content.");
 };
+
+
+const db = require("../models");
+const User = db.User;
+
+// Find a single User with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  User.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found User with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving User with id=" + id });
+    });
+};
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update User with id=${id}. Maybe User was not found!`
+        });
+      } else res.send({ message: "User was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id
+      });
+    });
+};
+
+// Delete a User with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  User.findByIdAndRemove(id, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete User with id=${id}. Maybe User was not found!`
+        });
+      } else {
+        res.send({
+          message: "User was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete User with id=" + id
+      });
+    });
+};
+
+// Delete all User from the database.
+exports.deleteAll = (req, res) => {
+  User.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Tutorials were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    });
+};
+
+// Find all  Active User
+exports.findAllUserActived = (req, res) => {
+  User.find({ active: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving user."
+      });
+    });
+};
+
+
+
+// Retrieve all User from the database.
+exports.findAllByUser = (req, res) => {
+  User.find({})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving user data."
+      });
+    });
+};
